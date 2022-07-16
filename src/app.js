9200be3125bb3278c8d1d95f00ddbfa56aab6409
Stévan Corre <stevancorre@protonmail.com>
2022-07-16 16:12:42 +0200

    simple express server setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log("Ok");
+import express from "express";
+
+const app = express();
+
+app.use((_, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    );
+    next();
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+export default app;
